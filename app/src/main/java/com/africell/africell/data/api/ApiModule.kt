58985bf76e57fded9f57ada31733074881726c09
denise <denise.zahreddine@tedmob.com>
2021-10-07 14:17:02 +0300

    * change api credentials

--- app/src/main/java/com/africell/africell/data/api/ApiModule.kt
+++ app/src/main/java/com/africell/africell/data/api/ApiModule.kt
@@ -66,7 +66,7 @@ object ApiModule {
         }*/
 
         val block: (chain: Interceptor.Chain) -> Response = {
-            val credentials: String = Credentials.basic("TestingAPI", "TestingAPI")
+            val credentials: String = Credentials.basic("sc-afr-gm-api", "s@c_2hg!0m9k")
 
             val response = it.proceed(
                 it.request().let { request ->
@@ -150,7 +150,7 @@ object ApiModule {
         //.connectionSpecs(listOf(ConnectionSpec.COMPATIBLE_TLS))
 
         builder.addInterceptor {//addNetworkInterceptor in case you don't want to handle redirections, but logs will not show "access-token"
-            val credentials: String = Credentials.basic("TestingAPI", "TestingAPI", UTF_8)
+         //   val credentials: String = Credentials.basic("TestingAPI", "TestingAPI", UTF_8)
             it.proceed(
                 it.request().let { request ->
                     request.newBuilder()

a38ff7654fbcbcefb3d701b5aeab7f07b6faf6bc
alfredabdo13 <alfredabdo13@gmail.com>
2023-04-10 12:37:47 +0300

    Added missing CreationExtras

--- app/src/main/java/com/africell/africell/ui/viewmodel/CustomViewModelLazy.kt
+++ app/src/main/java/com/africell/africell/ui/viewmodel/CustomViewModelLazy.kt
@@ -3,13 +3,15 @@ package com.africell.africell.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.viewmodel.CreationExtras
 import kotlin.reflect.KClass
 
 class CustomViewModelLazy<VM : ViewModel>(
     private val viewModelClass: KClass<VM>,
     private val keyProducer: (() -> String?)?,
     private val storeProducer: () -> ViewModelStore,
-    private val factoryProducer: () -> ViewModelProvider.Factory
+    private val factoryProducer: () -> ViewModelProvider.Factory,
+    private val creationExtrasProducer: () -> CreationExtras,
 ) : Lazy<VM> {
     private var cached: VM? = null
 
@@ -19,12 +21,13 @@ class CustomViewModelLazy<VM : ViewModel>(
             return if (viewModel == null) {
                 val factory = factoryProducer()
                 val store = storeProducer()
+                val creationExtras = creationExtrasProducer()
 
                 run {
                     keyProducer?.invoke()?.let {
-                        ViewModelProvider(store, factory)[it, viewModelClass.java]
+                        ViewModelProvider(store, factory, creationExtras)[it, viewModelClass.java]
                     }
-                        ?: ViewModelProvider(store, factory)[viewModelClass.java]
+                        ?: ViewModelProvider(store, factory, creationExtras)[viewModelClass.java]
                 }.also {
                     cached = it
                 }

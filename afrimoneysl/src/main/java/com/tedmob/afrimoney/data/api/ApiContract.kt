348593d37c207f7fd4706180ccd8dd7688b0b494
Mohamad Tafesh <tafesh.mohamad10@gmail.com>
2023-07-31 15:43:23 +0300

    update afrimoney base url

--- afrimoneysl/src/main/java/com/tedmob/afrimoney/data/api/ApiContract.kt
+++ afrimoneysl/src/main/java/com/tedmob/afrimoney/data/api/ApiContract.kt
@@ -1,5 +1,6 @@
 package com.tedmob.afrimoney.data.api
 
 object ApiContract {
-    const val BASE_URL = "https://api.sandbox.afrimoney.sl/v1.2/mobile/"
+  //TEST  const val BASE_URL = "https://api.sandbox.afrimoney.sl/v1.2/mobile/"
+    const val BASE_URL = "https://api.afrimoney.sl/v1.2/mobile/"
 }

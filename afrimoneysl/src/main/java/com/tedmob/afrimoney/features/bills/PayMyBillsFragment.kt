e11a082d81f17e038f397f5f8fc779b1b3ddf7db
Mohamad Tafesh <tafesh.mohamad10@gmail.com>
2024-04-07 15:57:39 +0300

    working on gambia nawec

--- afrimoneysl/src/main/java/com/tedmob/afrimoney/features/bills/PayMyBillsFragment.kt
+++ afrimoneysl/src/main/java/com/tedmob/afrimoney/features/bills/PayMyBillsFragment.kt
@@ -1,31 +1,16 @@
 package com.tedmob.afrimoney.features.bills
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import coil.load
 import coil.transform.CircleCropTransformation
-import com.google.android.gms.maps.model.Circle
 import com.tedmob.afrimoney.R
-import com.tedmob.afrimoney.app.BaseActivity
 import com.tedmob.afrimoney.app.BaseVBFragment
 import com.tedmob.afrimoney.app.withVBAvailable
-import com.tedmob.afrimoney.data.entity.Bank
-import com.tedmob.afrimoney.data.entity.PayMyBillServiceItem
 import com.tedmob.afrimoney.databinding.FragmentPayMyBillsBinding
-import com.tedmob.afrimoney.databinding.ItemPayMyBillServiceBinding
-import com.tedmob.afrimoney.databinding.ToolbarCustomPayMyBillsBinding
-import com.tedmob.afrimoney.features.banking.BankingServicesFragmentDirections
-import com.tedmob.afrimoney.ui.applySheetEffectTo
-import com.tedmob.afrimoney.ui.button.setDebouncedOnClickListener
-import com.tedmob.afrimoney.ui.viewmodel.observeResourceInline
-import com.tedmob.afrimoney.ui.viewmodel.provideNavGraphViewModel
-import com.tedmob.afrimoney.util.adapter.adapter
-import com.tedmob.afrimoney.util.navigation.runIfFrom
 import dagger.hilt.android.AndroidEntryPoint
 
 @Suppress("UNREACHABLE_CODE")
@@ -35,7 +20,7 @@ class PayMyBillsFragment : BaseVBFragment<FragmentPayMyBillsBinding>() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return createViewBinding(container, FragmentPayMyBillsBinding::inflate)
-        }
+    }
 
     override fun configureToolbar() {
         actionbar?.hide()
@@ -52,53 +37,51 @@ class PayMyBillsFragment : BaseVBFragment<FragmentPayMyBillsBinding>() {
             }
 
 
-            img1.load(R.drawable.ic_pay_my_bills_postpaid){
+            img1.load(R.drawable.ic_nawec) {
                 transformations(CircleCropTransformation())
             }
-            img2.load(R.drawable.paymybillsedsa){
-                transformations(CircleCropTransformation())
-            }
-            img3.load(R.drawable.paymybillsdstv){
-                transformations(CircleCropTransformation())
-            }
+            /* img2.load(R.drawable.paymybillsedsa){
+                 transformations(CircleCropTransformation())
+             }
+             img3.load(R.drawable.paymybillsdstv){
+                 transformations(CircleCropTransformation())
+             }
 
-            img4.load(R.drawable.paymybillsrisingacadamy){
-                transformations(CircleCropTransformation())
-            }
-            img5.load(R.drawable.paymybillsmercury){
-                transformations(CircleCropTransformation())
-            }
-            img6.load(R.drawable.paymybillsfcc){
-                transformations(CircleCropTransformation())
-            }
-            img7.load(R.drawable.paymybillswaec){
-                transformations(CircleCropTransformation())
-            }
-            img8.load(R.drawable.paymybillspowergen){
-                transformations(CircleCropTransformation())
-            }
-            img9.load(R.drawable.paymybillscovidtest){
-                transformations(CircleCropTransformation())
-            }
+             img4.load(R.drawable.paymybillsrisingacadamy){
+                 transformations(CircleCropTransformation())
+             }
+             img5.load(R.drawable.paymybillsmercury){
+                 transformations(CircleCropTransformation())
+             }
+             img6.load(R.drawable.paymybillsfcc){
+                 transformations(CircleCropTransformation())
+             }
+             img7.load(R.drawable.paymybillswaec){
+                 transformations(CircleCropTransformation())
+             }
+             img8.load(R.drawable.paymybillspowergen){
+                 transformations(CircleCropTransformation())
+             }
+             img9.load(R.drawable.paymybillscovidtest){
+                 transformations(CircleCropTransformation())
+             }*/
 
-            edsa.setOnClickListener{findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavEdsa())}
+            nawec.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavNawec()) }
 
-            africellPostPaid.setOnClickListener{findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavPostpaid())}
+            /*   africellPostPaid.setOnClickListener{findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavPostpaid())}
 
-            dstv.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToChooseMyBillServiceTypeFragment()) }
+               dstv.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToChooseMyBillServiceTypeFragment()) }
 
-            risingAcademy.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavRisingAcademy()) }
+               risingAcademy.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavRisingAcademy()) }
 
-            mercury.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavMercury()) }
+               mercury.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavMercury()) }
 
-            waec.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavWaec()) }
+               waec.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavWaec()) }
 
-            fcc.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavFcc()) }
+               fcc.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavFcc()) }
 
-            powergen.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavPowergen()) }
+               powergen.setOnClickListener { findNavController().navigate(PayMyBillsFragmentDirections.actionPayMyBillsFragmentToNavPowergen()) }*/
         }
-
-
 
 
     }
